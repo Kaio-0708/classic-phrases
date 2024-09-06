a6fc4f2157546508734f24c13477e7acf960785e
Kaio0708 <kaiovitor0707@gmail.com>
2024-09-05 21:14:35 -0300

    first commit

---
+++ screenmatch/src/main/java/br/com/kaio/screenmatch/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.kaio.screenmatch;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
